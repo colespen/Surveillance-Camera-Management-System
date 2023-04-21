2215793c813f9490cf4ca6b49a8a4d12bf7ce472
colespen <spencerlewiscole@gmail.com>
2023-04-20 23:12:30 -0400

    login credentials with next-auth

--- pages/api/uploadVideo.ts
+++ pages/api/uploadVideo.ts
@@ -1,36 +1,36 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import AWS from "aws-sdk";
-import stream from "stream";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import AWS from "aws-sdk";
+// import stream from "stream";
 
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
-});
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+//   region: process.env.AWS_REGION,
+// });
 
-const s3 = new AWS.S3();
+// const s3 = new AWS.S3();
 
-const params = {
-  Bucket: "surveillance-management-system",
-  Key: "loading-bay-1.mp4",
-  // Key: "https://surveillance-management-system.s3.us-east-2.amazonaws.com/loading-bay-1.mov",
-};
+// const params = {
+//   Bucket: "surveillance-management-system",
+//   Key: "loading-bay-1.mp4",
+//   // Key: "https://surveillance-management-system.s3.us-east-2.amazonaws.com/loading-bay-1.mov",
+// };
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  try {
-    const { Body } = await s3.getObject(params).promise();
-    const videoStream = new stream.PassThrough();
-    videoStream.end(Body);
-    res.setHeader("Content-Type", "video/mp4");
-    videoStream.pipe(res);
-    
-    // console.log("videoStream.pipe(res): ", videoStream.pipe(res));
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   try {
+//     const { Body } = await s3.getObject(params).promise();
+//     const videoStream = new stream.PassThrough();
+//     videoStream.end(Body);
+//     res.setHeader("Content-Type", "video/mp4");
+//     videoStream.pipe(res);
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-}
+//     // console.log("videoStream.pipe(res): ", videoStream.pipe(res));
+
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,44 +1,33 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { getSession, useSession } from "next-auth/react";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, GetStaticPathsResult, GetStaticProps } from "next";
 import Layout from "../components/Layout";
+import { Session } from "next-auth";
 
-// TODO: types!
+// TODO: url[]
 type DashboardProps = {
-  data: any;
-  session: any;
-};
-
-export const getServerSideProps: GetServerSideProps<DashboardProps> = async (
-  ctx
-) => {
-  const session = await getSession(ctx);
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/api/auth/signin",
-        permanent: false,
-      },
-    };
-  }
-  const res = await fetch("http://localhost:3000/api/uploadVideo");
-  // const data = await res.json();
-  const data = { url: URL.createObjectURL(await res.blob()) };
-  // console.log("data: ", data)
-
-  return {
-    props: {
-      data,
-      session,
-    },
+  data: {
+    url1: string,
+    url2: string
   };
 };
 
-const Dashboard: React.FC<DashboardProps> = ({ data, session }) => {
+export const getStaticProps: GetStaticProps<DashboardProps> = async () => {
+  const videoFeeds = {
+    url1: "https://dunz8t440z7z8.cloudfront.net/loading-bay-1.mp4",
+    url2: "https://dunz8t440z7z8.cloudfront.net/loading-bay-2.mp4",
+  };
+
+  return {
+    props: { data: videoFeeds },
+  };
+};
+
+const Dashboard: React.FC<DashboardProps> = ({ data }) => {
   const { push } = useRouter();
-  const { status } = useSession({
+  // session is Session / undefined / null
+  const { status, data: session } = useSession({
     required: true,
     onUnauthenticated: () => {
       push("/api/auth/signin");
@@ -51,10 +40,10 @@ const Dashboard: React.FC<DashboardProps> = ({ data, session }) => {
 
   if (!session) {
     return (
-      <Layout>
+      <>
         <h1>Restricted Access</h1>
         <div>You need to be authenticated to view this page.</div>
-      </Layout>
+      </>
     );
   }
 
@@ -62,12 +51,14 @@ const Dashboard: React.FC<DashboardProps> = ({ data, session }) => {
     <Layout>
       <div className="page">
         <h1>Surveillance Management System</h1>
+
+        {/* // TODO: .map() VIDEOS nad associated urls */}
         <video controls width="543" height="305" autoPlay loop>
-          <source
-            src="./loading-bay-1.mp4"
-            type="video/mp4"
-            // src={data.url}
-          />
+          <source type="video/mp4" src={data.url1} />
+          Your browser does not support video html element.
+        </video>
+        <video controls width="543" height="305" autoPlay loop>
+          <source type="video/mp4" src={data.url2} />
           Your browser does not support video html element.
         </video>
       </div>

62da58a94a8aee7759e4f25973103597a7661fe6
Spencer Cole <spencerlewiscole@gmail.com>
2023-05-06 13:26:04 -0400

    cleanup for production build

--- pages/api/alerts.ts
+++ pages/api/alerts.ts
@@ -2,7 +2,12 @@ import prisma from "../../lib/prisma";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { AlertType } from "@prisma/client";
 
-interface AlertCreateInput {
+// interface AlertCreateInput {
+//   cameraId: number;
+//   type: AlertType;
+// }
+
+interface AlertRequestBody {
   cameraId: number;
   type: AlertType;
 }
@@ -19,12 +24,12 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       if (!req.body) {
         res.status(400).json({ message: "alert couldn't be created" });
       }
-      const { cameraId, type } = req.body;
+      const { cameraId, type } = req.body as AlertRequestBody;
       const newAlert = await prisma.alert.create({
         data: {
           cameraId,
           type,
-        } as AlertCreateInput,
+        },
       });
       console.log("alerted created: ", newAlert);
       res.status(200).json(newAlert)
